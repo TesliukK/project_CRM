c30410b5432122e727ab056efa0b7c386e3d5a4e
TesliukK <teslukkirill@gmail.com>
2023-10-27 11:40:38 +0300

    fix item services

--- backend/src/services/item.service.ts
+++ backend/src/services/item.service.ts
@@ -6,74 +6,48 @@ class ItemService {
   public async getAllAndSearch(query: IQuery): Promise<IPaginationResponse<IItem>> {
     try {
       const queryStr = JSON.stringify(query);
-      const queryObj = JSON.parse(
-        queryStr.replace(/\b(gte|lte|gt|lt)\b/, (match) => `$${match}`),
-      );
+      const queryObj = JSON.parse(queryStr.replace(/\b(gte|lte|gt|lt)\b/, (match) => `$${match}`));
 
-      const {
-        page = 1,
-        limit = 10,
-        sortedBy = "createdAt",
-        search = "",
-        ...searchObject
-      } = queryObj;
-
+      const { page = 1, limit = 10, sortedBy = "createdAt", search = "", ...searchObject } = queryObj;
       const skip = limit * (page - 1);
 
-      if (!search.trim()) {
-        // Якщо пошук порожній, повертаємо всі айтеми без будь-яких додаткових фільтрів
-        const items = await Items.find(searchObject)
-          .skip(skip)
-          .limit(limit)
-          .sort(sortedBy)
-          .lean();
-        const itemsTotalCount = await Items.countDocuments(searchObject);
-        const totalPages = Math.ceil(itemsTotalCount / limit);
-        return {
-          page: +page,
-          totalPages: totalPages,
-          itemsCount: itemsTotalCount,
-          itemsFound: items.length,
-          perPage: +limit,
-          data: items.slice(0, limit),
+      let searchQuery: any = searchObject;
+      if (search.trim()) {
+        const searchWords: string[] = search.split(" ").filter((word: string) => word.trim() !== "");
+        searchQuery = {
+          ...searchObject,
+          $and: searchWords.map((word: string) => ({
+            $or: [
+              { itemName: { $regex: word, $options: "i" } },
+              { brand: { $regex: word, $options: "i" } },
+              { article: { $regex: word, $options: "i" } },
+              { size: { $regex: word, $options: "i" } },
+            ],
+          })),
         };
       }
 
-      const searchWords: string[] = search
-        .split(" ")
-        .filter((word: string) => word.trim() !== "");
+      const [items, itemsTotalCount] = await Promise.all([
+        Items.find(searchQuery).skip(skip).limit(limit).sort(sortedBy).lean(),
+        Items.countDocuments(searchQuery),
+      ]);
 
-      const searchQuery = {
-        ...searchObject,
-        $and: searchWords.map((word: string) => ({
-          $or: [
-            {itemName: {$regex: word, $options: "i"}},
-            {brand: {$regex: word, $options: "i"}},
-            {article: {$regex: word, $options: "i"}},
-            {size: {$regex: word, $options: "i"}},
-          ],
-        })),
-      };
-
-      const items = await Items.find(searchQuery)
-        .skip(skip)
-        .limit(limit)
-        .sort(sortedBy)
-        .lean();
-      const itemsTotalCount = await Items.countDocuments(searchQuery);
       const totalPages = Math.ceil(itemsTotalCount / limit);
+      const slicedItems = items.slice(0, limit);
+
       return {
         page: +page,
-        totalPages: totalPages,
+        totalPages,
         itemsCount: itemsTotalCount,
-        itemsFound: items.length,
+        itemsFound: slicedItems.length,
         perPage: +limit,
-        data: items.slice(0, limit),
+        data: slicedItems,
       };
     } catch (e) {
       throw new ApiError(e.message, e.status);
     }
   }
+
 
 
   public async create(data: IItem): Promise<any> {
